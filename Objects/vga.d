6e5343b617d069661a022bca1e93b8fa610cf40b
PatrickLeclerc <45087613+PatrickLeclerc@users.noreply.github.com>
2022-03-24 23:11:55 -0400

    All vga char implemented

--- Objects/vga.d
+++ Objects/vga.d
@@ -1,2 +1,2 @@
 ./objects/vga.o: vga.c vga.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h vga_char.h
--- vga.c
+++ vga.c
@@ -1,10 +1,19 @@
 #include "vga.h"
 void updateBuffer(uint16_t* table, uint32_t tableSize, uint32_t line){
 	initTable(table,tableSize,0);
-	uint8_t* character = charToVga('D');
-	if(line<8){
-		table[2] |= character[line];
-	}
+	uint8_t* character1 = charToVga('!');
+	uint8_t* character2 = charToVga('a');
+	uint8_t* character3 = charToVga('A');
+	//if(line<8){
+		table[2] |= character1[line%8];
+		table[3] |= character2[line%8];
+		table[4] |= character3[line%8];
+	//}
+}
+
+uint8_t* charToVga(char val){
+	if(val<32) return vgaChar[' '-32];
+	else	return vgaChar[val-32];
 }
 
 void initTable(uint16_t* table, uint32_t tableSize, uint16_t value){
@@ -14,9 +23,4 @@ void initTable(uint16_t* table, uint32_t tableSize, uint16_t value){
 		table[i]=value;
 	}
 	table[tableSize-1]=0x0F5FU*marge;
-}
-
-
-uint8_t* charToVga(char val){
-	return vgaChar[val-65];
 }
